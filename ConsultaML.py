6c3cf9ab4e8d29f128aa9d80774e32fe32ca0047
Ronaldo Montan <ronaldogithub@gmail.com>
2022-07-19 21:46:09 -0300

    varios resultados de busca no mercado libre com opção de escolha do produto

--- ConsultaML.py
+++ ConsultaML.py
@@ -8,9 +8,27 @@ response = requests.get(url_base + busca)
 
 site = BeautifulSoup(response.text, 'html.parser')
 
-produto = site.find('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'})
+produtos = site.findAll('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'})
 
-titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
+for produto in produtos:
 
-print(produto.prettify())
-print(f'Titulo do produto é {titulo.text}')
+    titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
+
+    link = produto.find('a', attrs={'class': 'ui-search-link'})
+
+    moeda = produto.find('span', attrs={'class': 'price-tag-symbol'})
+    real = produto.find('span', attrs={'class': 'price-tag-fraction'})
+    centavos = produto.find('span', attrs={'class': 'price-tag-cents'})
+    valor = str(moeda.text + real.text + ',' + centavos.text)
+
+    #print(produto.prettify())
+    print()
+    print(url_base + busca)
+    print(f'Titulo do produto é {titulo.text}')
+
+    if (centavos):
+        print(f'o Valor do produto é {valor}')
+    else:
+        print('O valor do produto é ', moeda.text + real.text)    
+    print('O link do produto é ', link['href'])
+    print('\n----------------------------------------\n')
